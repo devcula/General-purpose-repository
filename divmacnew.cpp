df6ae37d47d6d6d4e3ae47314a70fcfc59fa7fe7
Abhishek Prasad <abhimaxfield@gmail.com>
2016-09-05 13:46:46 +0530

    Update divmacnew.cpp

--- divmacnew.cpp
+++ divmacnew.cpp
@@ -1,21 +1,66 @@
 #include<iostream>
 #include<math.h>
 #include<stdlib.h>
+#include<vector>
 using namespace std;
-#define length 1000000
-int a[length];
-
+vector<int64_t> a;
+vector<int64_t> primes;
+void prime_generator()
+{
+    int last=a[0];
+    for(int64_t i=0;i<a.size();i++)
+        if(a[i]>last)
+        last=a[i];
+    if(primes.size()==0)
+    {
+        primes.push_back(2);
+    for(int j=3;j<last/2;j+=2)
+    {
+        int counter=1;
+        for(int k=3;k<=sqrt(j);k+=2)
+        {
+            if(j%k==0)
+            {
+                counter=0;
+                break;
+            }
+        }
+        if(counter)
+            primes.push_back(j);
+    }
+    }
+    else if(last/2>primes[primes.size()-1])
+    {
+        for(int64_t i=primes[primes.size()-1]+1;i<=last/2;i++)
+        {
+            int counter=1;
+            for(int64_t j=3;j<sqrt(i);j+=2)
+            {
+                if(i%j==0)
+            {
+                counter=0;
+                break;
+            }
+            }
+            if(counter)
+                primes.push_back(i);
+        }
+    }
+}
 int64_t LeastPrimeDivisor(int64_t num)
 {
-    if(num%2==0)
+    if(num==1)
+        return 1;
+    else if(num%2==0)
         return 2;
     else
+    for(int64_t i=1;i<primes.size();i++)
     {
-        len=num/2+1;
-        bool primes[len];
-        for(int64_t i=1;i<len;i++)
-            primes[i]=true;
-        
+        if(num%primes[i]==0)
+        {
+            return primes[i];
+            break;
+        }
     }
 }
 
@@ -49,7 +94,12 @@ int main()
         int64_t sizeofarray,queries;
         cin>>sizeofarray>>queries;
         for(int64_t i=0;i<sizeofarray;i++)
-            cin>>a[i];
+            {
+                int64_t temp;
+                cin>>temp;
+                a.push_back(temp);
+            }
+        prime_generator();
         for(int j=0;j<queries;j++)
         {
             int type;
@@ -61,6 +111,7 @@ int main()
             else
                 get(l-1,r-1);
         }
+        a.clear();
     }
     return 0;
 }
